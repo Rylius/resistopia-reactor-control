2437725011da764bf0974bc02db08555010547c2
Rylius <ryliusk@gmail.com>
2017-08-04 21:12:49 +0200

    Initial project structure

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app">
+        <h1>Reaktor-/Wassersimulation</h1>
+        <router-view></router-view>
+    </div>
+</template>
+
+<style lang="less">
+    @import "assets/less/stylesheet";
+</style>
+
+<script>
+    export default {
+        name: 'app',
+    };
+</script>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,52 @@
+<template>
+    <section class="content">
+        <h2>Übersicht</h2>
+
+        <div>
+            <button class="danger" @click="reset">Reset</button>
+
+            <router-link :to="{name: 'backups'}">Alle Backups</router-link>
+        </div>
+
+        <div>
+            <button class="danger" :disabled="!state.global.effects" @click="disableEffects">OT</button>
+            <button class="danger" :disabled="state.global.effects" @click="enableEffects">IT</button>
+        </div>
+
+        <div>
+            <ul>
+                <li>
+                    <router-link :to="{name: 'modify-simulation'}">Simulation bearbeiten</router-link>
+                </li>
+                <li>
+                    <router-link :to="{name: 'edit-safe-state'}">Sicheren Zustand bearbeiten</router-link>
+                </li>
+            </ul>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'dashboard',
+        data() {
+            return {
+                state: {
+                    stateMachines: {},
+                    global: {},
+                },
+            };
+        },
+        methods: {
+            reset() {
+                // TODO
+            },
+            enableEffects() {
+                // TODO
+            },
+            disableEffects() {
+                // TODO
+            },
+        },
+    };
+</script>
